dfd040de973766d85cb20bbc4a0842954ecb4f2b
pedrobarcena-py <pedroluisbarcenaperez@gmail.com>
2026-05-09 01:03:32 -0500

    'v2'

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API = "http://127.0.0.1:8000";
+const API = "https://precardi.onrender.com";
 
 function mostrarTab(nombre, btn) {
     document.querySelectorAll('.tab-content').forEach(t => t.classList.remove('active'));
